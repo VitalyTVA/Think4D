e4c7165e950d81e4d65f8ad812cc879c9bad31d7
Vitaly Tolstikov <vitalytva@yandex.ru>
2016-03-11 23:23:04 +0400

    Init

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class Game : MonoBehaviour {
+    public GameObject vertex;
+    public GameObject edge;
+    public float rotationSpeed = 1;
+    // Use this for initialization
+
+    //Vector3[] vertexPoints = new Vector3[] {
+    //        new Vector3(1, 1, 1),
+    //        new Vector3(1, 1, -1),
+    //        new Vector3(1, -1, 1),
+    //        new Vector3(1, -1, -1),
+    //        new Vector3(-1, 1, 1),
+    //        new Vector3(-1, 1, -1),
+    //        new Vector3(-1, -1, 1),
+    //        new Vector3(-1, -1, -1),
+    //    };
+    GameObject[] vertexes, edges;
+    GameObject root;
+    void Start() {
+        //polyhedron = Polyhedron.Cube3D;
+        var polyhedron = GetPoly(Matrix4x4.identity);
+
+        root = new GameObject("Cube");
+        root.transform.position = Vector3.zero;
+        //cube.transform.rotation = Quaternion.Euler(40, 40, 40);
+
+        vertexes = polyhedron.Vertexes.Select(x => AddVertex(vertex, root)).ToArray();
+        edges = polyhedron.Edges.Select(x => AddEdge(edge, root)).ToArray();
+    }
+
+    static Polyhedron<Vector3> GetPoly(Matrix4x4 m) {
+        return Polyhedron.Cube4D.FMap(x => m * x).Project(new Vector4(0, 0, 0, 3), new HyperPlane4(Vector4.zero, new Vector4(0, 0, 0, 1)));
+    }
+
+    static GameObject AddVertex(GameObject prefab, GameObject parent) {
+        var vertex = (GameObject)Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        vertex.transform.parent = parent.transform;
+        return vertex;
+    }
+
+    static GameObject AddEdge(GameObject prefab, GameObject parent) {
+        var edge = (GameObject)Instantiate(prefab, Vector3.zero, Quaternion.identity);
+        edge.transform.parent = parent.transform;
+        return edge;
+    }
+
+    static void UpdateEdge(GameObject edge, Vector3 from, Vector3 to) {
+        var diff = to - from;
+        var len = diff.magnitude;
+        edge.transform.localPosition = (from + to) / 2;
+        edge.transform.localScale = new Vector3(edge.transform.localScale.x, len / 2, edge.transform.localScale.z);
+        edge.transform.localRotation = Quaternion.LookRotation(from - to, Vector3.up) * Quaternion.Euler(90, 0, 0);
+    }
+    // Update is called once per frame
+    float horz, vert, angleH, angleV;
+    void Update () {
+        if(Input.GetAxis("Fire1") == 0) {
+            horz += rotationSpeed * Input.GetAxis("Horizontal");
+            vert += rotationSpeed * Input.GetAxis("Vertical");
+        } else {
+            angleH += rotationSpeed * Input.GetAxis("Vertical");
+            angleV += rotationSpeed * Input.GetAxis("Horizontal");
+        }
+
+        root.transform.localRotation =
+            Quaternion.Euler(vert, 0, 0)
+            *
+            Quaternion.Euler(0, horz, 0);
+
+        Matrix4x4 m = GetM1() * GetM2();
+
+        var rotatedPolyhendron = GetPoly(m);
+
+        for(int i = 0; i < vertexes.Length; i++) {
+            vertexes[i].transform.localPosition = rotatedPolyhendron.Vertexes[i];
+        }
+        for(int i = 0; i < edges.Length; i++) {
+            UpdateEdge(edges[i], rotatedPolyhendron.Edges[i].Vertex1, rotatedPolyhendron.Edges[i].Vertex2);
+        }
+    }
+
+    private Matrix4x4 GetM1() {
+        var m = Matrix4x4.identity;
+        var c = Mathf.Cos(Mathf.PI * angleH / 180);
+        var s = Mathf.Sin(Mathf.PI * angleH / 180);
+        m[3, 3] = c;
+        m[1, 1] = c;
+        m[1, 3] = -s;
+        m[3, 1] = s;
+        return m;
+    }
+    //private Matrix4x4 GetM2() {
+    //    var m = Matrix4x4.identity;
+    //    var c = Mathf.Cos(Mathf.PI * angleV / 180);
+    //    var s = Mathf.Sin(Mathf.PI * angleV / 180);
+    //    m[3, 3] = c;
+    //    m[2, 2] = c;
+    //    m[2, 3] = -s;
+    //    m[3, 2] = s;
+    //    return m;
+    //}
+    private Matrix4x4 GetM2() {
+        var m = Matrix4x4.identity;
+        var c = Mathf.Cos(Mathf.PI * angleV / 180);
+        var s = Mathf.Sin(Mathf.PI * angleV / 180);
+        m[0, 0] = c;
+        m[1, 1] = c;
+        m[1, 0] = -s;
+        m[0, 1] = s;
+        return m;
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Void {
+    public static readonly Void Instance = new Void();
+    Void() { }
+}
+public class Edge<T> {
+    public readonly T Vertex1, Vertex2;
+    public Edge(T vertex1, T vertex2) {
+        Vertex1 = vertex1;
+        Vertex2 = vertex2;
+    }
+    public Edge<TNew> FMap<TNew>(Func<T, TNew> map) {
+        return new Edge<TNew>(map(Vertex1), map(Vertex2));
+    }
+}
+public class Polyhedron<T> {
+    public readonly ReadOnlyCollection<T> Vertexes;
+    public readonly ReadOnlyCollection<Edge<T>> Edges;
+    public Polyhedron(ReadOnlyCollection<T> vertexes, ReadOnlyCollection<Edge<T>> edges) {
+        Vertexes = vertexes;
+        Edges = edges;
+    }
+    public Polyhedron<TNew> FMap<TNew>(Func<T, TNew> map) {
+        return Polyhedron.Create(Vertexes.Select(map), Edges.Select(x => x.FMap(map)));
+    }
+}
+public static class Polyhedron {
+    public const float CubeSize = 1;
+
+    public static readonly Polyhedron<Void> Cube0D = Create(Void.Instance.Yield(), Enumerable.Empty<Edge<Void>>());
+   
+    public static readonly Polyhedron<float> Cube1D = MakePrism(Cube0D, (x, nextCoord) => nextCoord, CubeSize);
+
+    public static readonly Polyhedron<Vector2> Cube2D = MakePrism(Cube1D, (x, nextCoord) => new Vector2(x, nextCoord), CubeSize);
+
+    public static readonly Polyhedron<Vector3> Cube3D = MakePrism(Cube2D, (x, nextCoord) => new Vector3(x.x, x.y, nextCoord), CubeSize);
+
+    public static readonly Polyhedron<Vector4> Cube4D = MakePrism(Cube3D, (x, nextCoord) => new Vector4(x.x, x.y, x.z, nextCoord), CubeSize);
+   
+
+    public static Polyhedron<T> Create<T>(IEnumerable<T> vertexes, IEnumerable<Edge<T>> edges) {
+        return new Polyhedron<T>(vertexes.ToReadOnly(), edges.ToReadOnly());
+    }
+
+    public static Polyhedron<TNPlus1> MakePrism<TN, TNPlus1>(this Polyhedron<TN> polyhedron, Func<TN, float, TNPlus1> addDimension, float newDimensionSize) {
+        var bottom = polyhedron.FMap((TN x) => addDimension(x, -newDimensionSize / 2));
+        var top = polyhedron.FMap((TN x) => addDimension(x, newDimensionSize / 2));
+        var newEdges = bottom.Vertexes.Zip(top.Vertexes, (v1, v2) => new Edge<TNPlus1>(v1, v2));
+        return Create(
+            bottom.Vertexes.Concat(top.Vertexes),
+            bottom.Edges.Concat(top.Edges).Concat(newEdges));
+    }
+
+    public static Vector3 Reduce(this Vector4 p) {
+        return new Vector3(p.x, p.y, p.z);
+    }
+
+    public static Polyhedron<Vector3> Project(this Polyhedron<Vector4> polyhedron, Vector4 projectionPoint, HyperPlane4 plane) {
+        return polyhedron.FMap(x => x.Project(projectionPoint, plane));
+    }
+    public static Vector3 Project(this Vector4 point, Vector4 projectionPoint, HyperPlane4 plane) {
+        var line = Line4.LineFromTo(point, projectionPoint);
+        var intersection = line.IntersectWith(plane);
+        return intersection.Reduce();
+    }
+}
+public static class LinqExtensions {
+    public static ReadOnlyCollection<T> ToReadOnly<T>(this IEnumerable<T> source) {
+        return new ReadOnlyCollection<T>(source.ToArray());
+    }
+    public static IEnumerable<T> Yield<T>(this T value) {
+        return new[] { value };
+    }
+    public static IEnumerable<TNew> Zip<T1, T2, TNew>(this IEnumerable<T1> en1, IEnumerable<T2> en2, Func<T1, T2, TNew> zip) {
+        var x1 = en1.GetEnumerator();
+        var x2 = en2.GetEnumerator();
+        while(true) {
+            var next1 = x1.MoveNext();
+            var next2 = x2.MoveNext();
+            if(next1 && next1) {
+                yield return zip(x1.Current, x2.Current);
+            } else if(!next1 && !next1) {
+                yield break;
+            } else {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
+public struct Line4 {
+    public static Line4 LineFromTo(Vector4 from, Vector4 to) {
+        return new Line4(from, from - to);
+    }
+
+    //Point + Direction * t
+    public readonly Vector4 Point;
+    public readonly Vector4 Direction;
+
+    public Line4(Vector4 point, Vector4 direction) {
+        Point = point;
+        Direction = direction;
+    }
+    public Vector4 IntersectWith(HyperPlane4 plane) {
+        var t = Vector4.Dot((plane.Point - Point), plane.Normal) / Vector4.Dot(Direction, plane.Normal);
+        return Point + Direction * t;
+    }
+}
+public struct HyperPlane4 {
+    //((x - Point), Normal) = 0
+    public readonly Vector4 Point;
+    public readonly Vector4 Normal;
+
+    public HyperPlane4(Vector4 point, Vector4 normal) {
+        Point = point;
+        Normal = normal;
+    }
+}
