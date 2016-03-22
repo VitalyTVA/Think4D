6acbe408a225fb65faef3c1c46f4efd944f9161a
Vitaly Tolstikov <vitalytva@yandex.ru>
2016-03-23 01:02:48 +0400

    t

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -43,32 +43,6 @@ public static class Polytope {
     }
 
 }
-public static class LinqExtensions {
-    public static ReadOnlyCollection<T> ToReadOnly<T>(this IEnumerable<T> source) {
-        return new ReadOnlyCollection<T>(source.ToArray());
-    }
-    public static IEnumerable<T> Yield<T>(this T value) {
-        return new[] { value };
-    }
-    public static IEnumerable<TNew> Zip<T1, T2, TNew>(this IEnumerable<T1> en1, IEnumerable<T2> en2, Func<T1, T2, TNew> zip) {
-        var x1 = en1.GetEnumerator();
-        var x2 = en2.GetEnumerator();
-        while(true) {
-            var next1 = x1.MoveNext();
-            var next2 = x2.MoveNext();
-            if(next1 && next1) {
-                yield return zip(x1.Current, x2.Current);
-            } else if(!next1 && !next1) {
-                yield break;
-            } else {
-                throw new InvalidOperationException();
-            }
-        }
-    }
-    public static IEnumerable<T> HeadToTail<T>(this IEnumerable<T> source) {
-        return source.Skip(1).Concat(source.Take(1));
-    }
-}
 public struct Line4 {
     public static Line4 LineFromTo(Vector4 from, Vector4 to) {
         return new Line4(from, from - to);
